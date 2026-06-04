ea3ba9698a4dd6286e69a971cc9858b733217288
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-04 09:54:58 +0300

    added two different names for each blog detail url blog-id & blog-slug

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", views.RecentBlogs.as_view(), name="index"),
     path("blogs/", views.AllBlogs.as_view(), name="blog-list"),
     path("my/", views.UserBlogs.as_view(), name="my"),
-    path('blogs/<int:blog_id>/', view=views.BlogDetail.as_view(), name='blog'),
-    path('blogs/<slug:slug>/', view=views.BlogDetail.as_view()),
+    path('blogs/<int:blog_id>/', view=views.BlogDetail.as_view(), name='blog-id'),
+    path('blogs/<slug:slug>/', view=views.BlogDetail.as_view(), name='blog-slug'),
 ]
